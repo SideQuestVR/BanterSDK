4bbcf998b33e3748555ecaf37a5348cbc77375dc
Shane Harris <shane@side.quest>
2024-06-03 08:59:36 +0200

    fixed the onscene loaded event

--- Runtime/Scripts/BanterLink/BanterLink.cs
+++ Runtime/Scripts/BanterLink/BanterLink.cs
@@ -101,11 +101,11 @@ namespace Banter.SDK
                     scene.SetLoaded();
                     return scene.loaded;
                 });
+                OnUnitySceneLoaded();
                 await Task.Delay(25000);
                 if(scene.state != SceneState.UNITY_READY) {
                     scene.LogMissing();
                 }
-                OnUnitySceneLoaded();
             }
             else
             {
