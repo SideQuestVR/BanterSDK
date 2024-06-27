808187927f6f9566a100ff5e3516505117b964bf
Mike Nisbet <mike@side.quest>
2024-06-27 16:29:29 +0100

    example

---
+++ Runtime/Scripts/VisualScripting/OnPlayerEnter.cs
@@ -0,0 +1,15 @@
+#if BANTER_VISUAL_SCRIPTING
+using UnityEngine;
+using Unity.VisualScripting;
+
+namespace Banter.VisualScripting
+{
+    public class OnPlayerEnter : Unit
+    {
+        protected override void Definition()
+        {
+
+        }
+    }
+}
+#endif
