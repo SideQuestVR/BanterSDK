d736984624e998051f8d1cfdbd8e888956ced5d6
Mike Nisbet <mike@side.quest>
2024-08-01 17:47:01 +0100

    TeleportTo

---
+++ VisualScripting/Space/TeleportTo.cs
@@ -0,0 +1,58 @@
+#if BANTER_VISUAL_SCRIPTING
+using UnityEngine;
+using Unity.VisualScripting;
+using Banter.SDK;
+using System.Linq;
+
+namespace Banter.VisualScripting
+{
+    [UnitTitle("Teleport To Location")]
+    [UnitShortTitle("Teleport")]
+    [UnitCategory("Banter")]
+    [TypeIcon(typeof(BanterObjectId))]
+    public class TeleportTo : Unit
+    {
+        [DoNotSerialize]
+        public ControlInput inputTrigger;
+
+        [DoNotSerialize]
+        public ControlOutput outputTrigger;
+
+        [DoNotSerialize]
+        public ValueInput targetPosition;
+
+        [DoNotSerialize]
+        public ValueInput targetRotation;
+
+        [DoNotSerialize]
+        public ValueInput stopVelocity;
+        [DoNotSerialize]
+        public ValueInput isSpawn;
+
+        protected override void Definition()
+        {
+            inputTrigger = ControlInput("", (flow) => {
+                var position = flow.GetValue<Vector3>(targetPosition);
+                var rotation = flow.GetValue<float>(targetRotation);
+                var stop = flow.GetValue<bool>(stopVelocity);
+                var spawn = flow.GetValue<bool>(isSpawn);
+
+                BanterScene.Instance().events.OnTeleport.Invoke(position, new Vector3(0f, rotation, 0f), stop, spawn);
+
+#if !BANTER_EDITOR // Local Teleport
+                var localPlayer = BanterScene.Instance().users.First(user => user.isLocal);
+                localPlayer.gameObject.transform.position = position;
+                localPlayer.gameObject.transform.eulerAngles = new Vector3(0f, rotation, 0f);
+#endif
+
+                return outputTrigger;
+            });
+            outputTrigger = ControlOutput("");
+            targetPosition = ValueInput("Position", Vector3.zero);
+            targetRotation = ValueInput("Rotation", 0f);
+            stopVelocity = ValueInput("Stop Velocity", false);
+            isSpawn = ValueInput("Is Spawn", false);
+        }
+    }
+}
+#endif
