6184aede2bda5cb88b85c4387f38fbea2cb03085
Mike Nisbet <mike@side.quest>
2024-08-06 15:22:03 +0100

    specific prop listener for sdk
    removes duplicate instance event in main build

--- Runtime/Scripts/BanterStarterUpper.cs
+++ Runtime/Scripts/BanterStarterUpper.cs
@@ -5,7 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SpatialTracking;
-
+#if BANTER_VISUAL_SCRIPTING
+using Unity.VisualScripting;
+#endif
 
 namespace Banter.SDK
 {
@@ -32,6 +34,7 @@ namespace Banter.SDK
             scene.mainThread = gameObject.AddComponent<UnityMainThreadDispatcher>();
 #if !BANTER_EDITOR
             localPlayerPrefab = Resources.Load<GameObject>("Prefabs/BanterPlayer");
+            SetupExtraEvents();
             SetupCamera();
             SpawnPlayers();
 #endif
@@ -96,6 +99,22 @@ namespace Banter.SDK
 #endif
             player.transform.position = spawnPoint;
             player.transform.eulerAngles = new Vector3(0, spawnRotation, 0);
+        }
+
+        void SetupExtraEvents()
+        {
+            scene.events.OnPublicSpaceStateChanged.AddListener((key, value) =>
+            {
+#if BANTER_VISUAL_SCRIPTING
+                EventBus.Trigger("OnSpaceStatePropsChanged", new CustomEventArgs(key, new object[] { value, false }));
+#endif
+            });
+            scene.events.OnProtectedSpaceStateChanged.AddListener((key, value) =>
+            {
+#if BANTER_VISUAL_SCRIPTING
+                EventBus.Trigger("OnSpaceStatePropsChanged", new CustomEventArgs(key, new object[] { value, true }));
+#endif
+            });
         }
 
         private void OnDisable()
