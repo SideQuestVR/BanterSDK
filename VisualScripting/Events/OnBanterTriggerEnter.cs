dccd83e59bba777df27eedd81a62f0a377ad0013
shane <shane@sidequestvr.com>
2024-11-16 21:32:18 +0100

    added onbantertriggerenter to also get the user

---
+++ VisualScripting/Events/OnBanterTriggerEnter.cs
@@ -0,0 +1,36 @@
+using System;
+using Banter.SDK;
+using UnityEngine;
+
+namespace Unity.VisualScripting
+{
+#if MODULE_PHYSICS_EXISTS
+    /// <summary>
+    /// Called when a collider enters the trigger.
+    /// </summary>
+    public sealed class OnBanterTriggerEnter : TriggerEventUnit
+    {
+        public override Type MessageListenerType => typeof(UnityOnTriggerEnterMessageListener);
+        protected override string hookName => EventHooks.OnTriggerEnter;
+        public new ValueOutput collider { get; private set; }
+        public ValueOutput user { get; private set; }
+        protected override void Definition()
+        {
+            base.Definition();
+
+            collider = ValueOutput<Collider>(nameof(collider));
+
+            user = ValueOutput<BanterUser>(nameof(user));
+        }
+        protected override void AssignArguments(Flow flow, Collider other)
+        {
+            flow.SetValue(collider, other);
+            var user = other.gameObject.GetComponentInParent<UserData>();
+            if (user != null)
+            {
+                flow.SetValue(this.user, new BanterUser() { name = user.name, id = user.id, uid = user.uid, color = user.color, isLocal = user.isLocal, isSpaceAdmin = user.isSpaceAdmin });
+            }
+        }
+    }
+#endif
+}
