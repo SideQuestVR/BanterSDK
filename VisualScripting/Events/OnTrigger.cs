eefbb1cb335e68e53c67ab246ccee979d089427c
shane <shane@sidequestvr.com>
2024-11-16 21:49:55 +0100

    added ontrigger and ontriggerpress

---
+++ VisualScripting/Events/OnTrigger.cs
@@ -0,0 +1,53 @@
+#if BANTER_VISUAL_SCRIPTING
+using Unity.VisualScripting;
+using Banter.SDK;
+using UnityEngine;
+
+namespace Banter.VisualScripting
+{
+
+    [UnitTitle("On Trigger BanterHeldEvent Event Received ")]
+    [UnitShortTitle("On Trigger BanterHeldEvent")]
+    [UnitCategory("Events\\Banter")]
+    [TypeIcon(typeof(BanterObjectId))]
+    public class OnTrigger : EventUnit<CustomEventArgs>
+    {
+        [DoNotSerialize]
+        [PortLabelHidden]
+        [NullMeansSelf]
+        public ValueInput gameObject { get; private set; }
+        [DoNotSerialize]
+        public ValueOutput isLeft;
+        [DoNotSerialize]
+        public ValueOutput input;
+
+        protected override bool register => true;
+
+        public override EventHook GetHook(GraphReference reference)
+        {
+            return new EventHook("OnTrigger");
+        }
+
+        protected override void Definition()
+        {
+            base.Definition();
+            // Setting the value on our port.
+            gameObject = ValueInput<GameObject>(nameof(gameObject), null).NullMeansSelf();
+            isLeft = ValueOutput<bool>("Is Left");
+            input = ValueOutput<float>("Input");
+        }
+
+        protected override bool ShouldTrigger(Flow flow, CustomEventArgs data)
+        {
+            return data.name == flow.GetValue<GameObject>(gameObject).GetInstanceID().ToString();
+        }
+
+        // Setting the value on our port.
+        protected override void AssignArguments(Flow flow, CustomEventArgs data)
+        {
+            flow.SetValue(input, data.arguments[0]);
+            flow.SetValue(isLeft, (HandSide)data.arguments[1] == HandSide.LEFT);
+        }
+    }
+}
+#endif
---
+++ VisualScripting/Events/OnTriggerPress.cs
@@ -0,0 +1,49 @@
+#if BANTER_VISUAL_SCRIPTING
+using Unity.VisualScripting;
+using Banter.SDK;
+using UnityEngine;
+
+namespace Banter.VisualScripting
+{
+
+    [UnitTitle("On TriggerPress BanterHeldEvent Event Received")]
+    [UnitShortTitle("On TriggerPress BanterHeldEvent")]
+    [UnitCategory("Events\\Banter")]
+    [TypeIcon(typeof(BanterObjectId))]
+    public class OnTriggerPress : EventUnit<CustomEventArgs>
+    {
+        [DoNotSerialize]
+        [PortLabelHidden]
+        [NullMeansSelf]
+        public ValueInput gameObject { get; private set; }
+        [DoNotSerialize]
+        public ValueOutput isLeft;
+
+        protected override bool register => true;
+
+        public override EventHook GetHook(GraphReference reference)
+        {
+            return new EventHook("OnTriggerPress");
+        }
+
+        protected override void Definition()
+        {
+            base.Definition();
+            // Setting the value on our port.
+            gameObject = ValueInput<GameObject>(nameof(gameObject), null).NullMeansSelf();
+            isLeft = ValueOutput<bool>("Is Left");
+        }
+
+        protected override bool ShouldTrigger(Flow flow, CustomEventArgs data)
+        {
+            return data.name == flow.GetValue<GameObject>(gameObject).GetInstanceID().ToString();
+        }
+
+        // Setting the value on our port.
+        protected override void AssignArguments(Flow flow, CustomEventArgs data)
+        {
+            flow.SetValue(isLeft, (HandSide)data.arguments[0] == HandSide.LEFT);
+        }
+    }
+}
+#endif
