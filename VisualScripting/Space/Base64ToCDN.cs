85fb7cad8218dab5ee9592c08d851425836f8b2a
shane <shane@sidequestvr.com>
2025-01-02 16:40:38 +0100

    vs stubs allowed

---
+++ VisualScripting/Space/Base64ToCDN.cs
@@ -0,0 +1,40 @@
+#if BANTER_VISUAL_SCRIPTING
+using UnityEngine;
+using Unity.VisualScripting;
+using Banter.SDK;
+using System.Linq;
+using Banter.Utilities.Async;
+
+namespace Banter.VisualScripting
+{
+    [UnitTitle("Base64 To CDN")]
+    [UnitShortTitle("Base64ToCDN")]
+    [UnitCategory("Banter")]
+    [TypeIcon(typeof(BanterObjectId))]
+    public class Base64ToCDN : Unit
+    {
+        [DoNotSerialize]
+        public ControlInput inputTrigger;
+    
+        [DoNotSerialize]
+        public ControlOutput outputTrigger;
+
+        [DoNotSerialize]
+        public ValueInput base64Image;
+
+        protected override void Definition()
+        {
+            inputTrigger = ControlInput("", (flow) => {
+                var _base64Image = flow.GetValue<string>(base64Image);
+                UnityMainThreadTaskScheduler.Default.Enqueue(() =>
+                {
+                    BanterScene.Instance().events.OnBase64ToCDN.Invoke(_base64Image);
+                });
+                return outputTrigger;
+            });
+            outputTrigger = ControlOutput("");
+            base64Image = ValueInput("Base64 String", "");
+        }
+    }
+}
+#endif
