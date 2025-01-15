d5a410d41c0bf86b5ed57b7d48e75c52dfa0ecd0
Mike Nisbet <mike@side.quest>
2025-01-15 14:17:54 +0000

    rename variable

--- VisualScripting/Utils/BanterGLTFHelpers.cs
+++ VisualScripting/Utils/BanterGLTFHelpers.cs
@@ -24,7 +24,7 @@ namespace Banter.VisualScripting
                 return gltfComp.ModelLoaded;
             });
 
-            gltfObject = ValueInput<BanterGLTF>("glTF Object", null);
+            gltfObject = ValueInput<BanterGLTF>("Banter GLTF", null);
             gltfObject.SetDefaultValue(null);
             gltfObject.NullMeansSelf();
         }
