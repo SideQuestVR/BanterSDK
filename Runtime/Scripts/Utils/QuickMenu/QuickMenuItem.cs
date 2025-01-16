de831b0a39e580def1b974a2e066f3e03aef5bf0
Gareth Williams <gareth@garethw.net>
2025-01-16 16:42:01 +0000

    Quickmenu support stuff

---
+++ Runtime/Scripts/Utils/QuickMenu/QuickMenuItem.cs
@@ -0,0 +1,44 @@
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Banter.SDK
+{
+    [Serializable]
+    public class QuickMenuItem : INotifyPropertyChanged {
+        public enum RadialMenuItemType
+        {
+            Basic,
+            Toggle
+        }
+
+        public RadialMenuItemType Type;
+        public string Label;
+        public Texture2D Icon;
+        public Texture2D Image;
+        public UnityEvent Click;
+        public QuickMenuItem[] Children;
+        public QuickMenuItem Parent;
+
+        public object Value
+        {
+            get => _value;
+            set 
+            { 
+                _value = value; 
+                OnPropertyChanged(); 
+            }
+        }
+    
+        private object _value;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
