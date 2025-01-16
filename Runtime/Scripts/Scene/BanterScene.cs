c2ecea649286ca9ffe16edea343063a1a159b085
shane <shane@sidequestvr.com>
2025-01-16 13:40:51 +0100

    de-germany

--- Runtime/Scripts/Scene/BanterScene.cs
+++ Runtime/Scripts/Scene/BanterScene.cs
@@ -346,7 +346,7 @@ namespace Banter.SDK
                     Debug.LogError("[Banter] AiModel message is malformed: " + msg);
                     return;
                 }
-                UnityMainThreadTaskScheduler.Default.Enqueue(() => events.OnAiModel.Invoke(parts[0], float.Parse(parts[1]), int.Parse(parts[1])));
+                UnityMainThreadTaskScheduler.Default.Enqueue(() => events.OnAiModel.Invoke(parts[0], NumberFormat.Parse(parts[1]), int.Parse(parts[1])));
             }catch(Exception e){
                 Debug.LogError(e);
             }
