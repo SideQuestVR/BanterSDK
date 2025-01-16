bdaac8b5702e0b9239fb4d0f1d69bb7aa844d43a
shane <shane@sidequestvr.com>
2025-01-16 12:24:17 +0100

    added namespace

--- Runtime/Scripts/Models/AiImageRatio.cs
+++ Runtime/Scripts/Models/AiImageRatio.cs
@@ -1,12 +1,15 @@
-public enum AiImageRatio
-{
-    _1_1,
-    _3_2,
-    _4_3,
-    _16_9,
-    _21_9,
-    _2_3,
-    _3_4,
-    _9_16,
-    _9_21
+namespace Banter.SDK {
+    public enum AiImageRatio
+    {
+        _1_1,
+        _3_2,
+        _4_3,
+        _16_9,
+        _21_9,
+        _2_3,
+        _3_4,
+        _9_16,
+        _9_21
+    }
+
 }
