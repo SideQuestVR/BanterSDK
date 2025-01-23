92e3ed96a3957b69b65598e1379c3b6f9690e44f
Mike Nisbet <mike@side.quest>
2025-01-23 10:27:24 +0000

    use public variables, comment out assemblyinfo

--- Runtime/AssemblyInfo.cs
+++ Runtime/AssemblyInfo.cs
@@ -1,5 +1,7 @@
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Assembly-CSharp-Editor")]
-[assembly: InternalsVisibleTo("Assembly-CSharp-Editor-firstpass")]
-[assembly: InternalsVisibleTo("Assembly-CSharp")]
+// Anything in the SDK marked internal can't be seen by main project without this line
+// Currently pointed at codegen variables to avoid requirement, but may need in future so keeping around.
+
+// Main banter project is just using the generic assembly? TODO
+// [assembly: InternalsVisibleTo("Assembly-CSharp")]
