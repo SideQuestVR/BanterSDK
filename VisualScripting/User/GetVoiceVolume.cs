222630fe61590e04f14a1ff522098a2708977bb6
shane <shane@sidequestvr.com>
2025-02-11 12:43:29 +0100

    added some more nodes

---
+++ VisualScripting/User/GetVoiceVolume.cs
@@ -0,0 +1,27 @@
+#if BANTER_VISUAL_SCRIPTING
+using UnityEngine;
+using Unity.VisualScripting;
+using Banter.SDK;
+using System.Linq;
+using Banter.Utilities.Async;
+
+namespace Banter.VisualScripting
+{
+    [UnitTitle("Get the voice volume of the Local User")]
+    [UnitShortTitle("GetVoiceVolume")]
+    [UnitCategory("Banter\\User")]
+    [TypeIcon(typeof(BanterObjectId))]
+    public class GetVoiceVolume : Unit
+    {
+        [DoNotSerialize]
+        public ValueOutput volume;
+
+        protected override void Definition()
+        {
+            volume = ValueOutput<float>("isFavourited", flow => {
+                return BanterStarterUpper.voiceVolume;
+            });
+        }
+    }
+}
+#endif
