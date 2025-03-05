ed4b3e830717086191b440f9b9e9605aa67afda3
shane <shane@sidequestvr.com>
2025-03-05 17:40:43 +0100

    added node to get current leaderboards

--- Runtime/Scripts/Scene/BanterSceneEvents.cs
+++ Runtime/Scripts/Scene/BanterSceneEvents.cs
@@ -57,6 +57,7 @@ public class BanterSceneEvents
     public UnityEvent<BanterWorldObject> OnWorldObjectCollectColliders = new UnityEvent<BanterWorldObject>();
     public UnityEvent<string, string> OnAvatarSet = new UnityEvent<string, string>();
     public UnityEvent<string, float, string> OnLeaderBoardScore = new UnityEvent<string, float, string>();
+    public UnityEvent OnGetLeaderBoard = new UnityEvent();
 
     #region Callback Functions
     public Func<string> GetUserLanguage = new Func<string>(() => { return ""; });
