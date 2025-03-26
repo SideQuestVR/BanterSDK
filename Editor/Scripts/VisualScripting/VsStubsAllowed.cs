c7599b01bb89159d17bf87077b01a8d2d3e14a69
Gareth Williams <gareth@garethw.net>
2025-03-26 13:39:06 +0000

    Stubs for PicaVoxel

--- Editor/Scripts/VisualScripting/VsStubsAllowed.cs
+++ Editor/Scripts/VisualScripting/VsStubsAllowed.cs
@@ -12775,6 +12775,7 @@ namespace Banter.SDKEditor
             "UnityEngine.Timeline.PlayableTrack.supportsNotifications",
             "UnityEngine.Timeline.PlayableTrack.name",
             "UnityEngine.Timeline.PlayableTrack.hideFlags",
+            "PicaVoxel.VisualScripting.OnVoxelManipulatorChange"
         };
     }
 }
