f763af258434f3fab52cf077eb32de68ce9a1aa1
shane <shane@sidequestvr.com>
2025-05-13 11:43:55 +0200

    added unescapeurl node

---
+++ VisualScripting/Utils/UnEscapeUrl.cs
@@ -0,0 +1,31 @@
+//
+
+#if BANTER_VISUAL_SCRIPTING
+using UnityEngine;
+using Unity.VisualScripting;
+using Banter.SDK;
+
+namespace Banter.VisualScripting
+{
+    [UnitTitle("UnEscape Url")]
+    [UnitShortTitle("UnEscape Url")]
+    [UnitCategory("Banter\\Utils")]
+    [TypeIcon(typeof(BanterObjectId))]
+    public class UnEscapeUrl : Unit
+    {
+        [DoNotSerialize]
+        public ValueInput inputString;
+
+        [DoNotSerialize]
+        public ValueOutput outputString;
+
+        private string fileContents;
+
+        protected override void Definition()
+        {
+            inputString = ValueInput<string>("Input Url");
+            outputString = ValueOutput("Output Url", (flow) => UnityWebRequest.UnEscapeURL(flow.GetValue<string>(inputString)));
+        }
+    }
+}
+#endif
