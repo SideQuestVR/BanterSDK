ce2995d62818fed172908c01b2220c8946b05e87
Gareth Williams <gareth@garethw.net>
2025-06-03 11:17:37 +0100

    Add GetSpaceURL node

---
+++ VisualScripting/Space/GetSpaceURL.cs
@@ -0,0 +1,24 @@
+﻿#if BANTER_VISUAL_SCRIPTING
+using Unity.VisualScripting;
+using Banter.SDK;
+
+namespace Banter.VisualScripting
+{
+    [UnitTitle("Get Space URL")]
+    [UnitShortTitle("Space URL")]
+    [UnitCategory("Banter\\Space")]
+    [TypeIcon(typeof(BanterObjectId))]
+    public class GetSpaceURL : Unit
+    {
+        [DoNotSerialize]
+        public ValueOutput spaceUrl;
+
+        protected override void Definition()
+        {
+            spaceUrl = ValueOutput<string>("spaceUrl", flow => {
+                return BanterScene.Instance().CurrentUrl;
+            });
+        }
+    }
+}
+#endif
