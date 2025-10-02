786d3ee25c2cc7d28000ae367566868725028677
Elin <37795467+Ladypoly@users.noreply.github.com>
2025-10-02 10:37:06 +0200

    Add Visual Scripting events for new UI controls (Radio Buttons, Slider Min Max)
    Introduces Visual Scripting event units for MinMaxSlider, RadioButton, RadioButtonGroup, and SliderInt UI controls. Updates UIElementBridge to support registration, unregistration, and event dispatch for these controls, including handling of int and Vector2 change events and mouse events.

---
+++ VisualScripting/UI/Events/OnRadioButtonGroupChanged.cs
@@ -0,0 +1,119 @@
+#if BANTER_VISUAL_SCRIPTING
+using Unity.VisualScripting;
+using Banter.SDK;
+using Banter.VisualScripting.UI.Helpers;
+
+namespace Banter.VisualScripting
+{
+    [UnitTitle("On Radio Button Group Changed")]
+    [UnitShortTitle("On Radio Button Group Changed")]
+    [UnitCategory("Events\\Banter\\UI\\Input")]
+    [TypeIcon(typeof(BanterObjectId))]
+    public class OnRadioButtonGroupChanged : EventUnit<CustomEventArgs>
+    {
+        [DoNotSerialize]
+        public ValueInput elementId;
+
+        [DoNotSerialize]
+        public ValueInput elementName;
+
+        [DoNotSerialize]
+        public ValueOutput changedElementId;
+
+        [DoNotSerialize]
+        public ValueOutput selectedValue;
+
+        [DoNotSerialize]
+        public ValueOutput selectedIndex;
+
+        protected override bool register => true;
+
+        public override EventHook GetHook(GraphReference reference)
+        {
+            return new EventHook("OnUIChange");
+        }
+
+        protected override void Definition()
+        {
+            base.Definition();
+            elementId = ValueInput<string>("Element ID", "");
+            elementName = ValueInput<string>("Element Name", "");
+            changedElementId = ValueOutput<string>("Element ID");
+            selectedValue = ValueOutput<string>("Selected Value");
+            selectedIndex = ValueOutput<int>("Selected Index");
+        }
+
+        protected override bool ShouldTrigger(Flow flow, CustomEventArgs data)
+        {
+            var targetId = flow.GetValue<string>(elementId);
+            var targetName = flow.GetValue<string>(elementName);
+
+            // Check if this is a change event
+            if (!data.name.StartsWith("UIChange_"))
+                return false;
+
+            // Priority: Element ID first, then Element Name
+            string resolvedTarget = UIElementResolverHelper.ResolveElementIdOrName(targetId, targetName);
+
+            // Extract element ID from event name
+            var eventElementId = data.name.Replace("UIChange_", "");
+
+            // If no specific element is provided, trigger for any int change (RadioButtonGroup typically uses int for index)
+            if (string.IsNullOrEmpty(resolvedTarget))
+            {
+                return data.arguments != null && data.arguments.Length >= 1 &&
+                       (data.arguments[0] is int ||
+                        (data.arguments[0] is string strVal && int.TryParse(strVal, out _)));
+            }
+
+            // Otherwise, only trigger for the specific element
+            return eventElementId == resolvedTarget &&
+                   data.arguments != null && data.arguments.Length >= 1 &&
+                   (data.arguments[0] is int ||
+                    (data.arguments[0] is string strVal2 && int.TryParse(strVal2, out _)));
+        }
+
+        protected override void AssignArguments(Flow flow, CustomEventArgs data)
+        {
+            // Extract element ID from event name
+            var eventElementId = data.name.Replace("UIChange_", "");
+            flow.SetValue(changedElementId, eventElementId);
+
+            // Parse the value - RadioButtonGroup typically returns an int index
+            if (data.arguments != null && data.arguments.Length >= 1)
+            {
+                int index = -1;
+                string value = "";
+
+                if (data.arguments[0] is int intValue)
+                {
+                    index = intValue;
+                    value = intValue.ToString();
+                }
+                else if (data.arguments[0] is string stringValue)
+                {
+                    if (int.TryParse(stringValue, out int parsedIndex))
+                    {
+                        index = parsedIndex;
+                        value = stringValue;
+                    }
+                    else
+                    {
+                        // If it's not an index, treat it as a value string
+                        value = stringValue;
+                        index = -1;
+                    }
+                }
+
+                flow.SetValue(selectedIndex, index);
+                flow.SetValue(selectedValue, value);
+            }
+            else
+            {
+                flow.SetValue(selectedIndex, -1);
+                flow.SetValue(selectedValue, "");
+            }
+        }
+    }
+}
+#endif
