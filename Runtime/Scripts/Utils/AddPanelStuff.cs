c73b49cab34274e966be5755de468df7ba34d91b
Gareth Williams <gareth@garethw.net>
2025-10-09 16:54:19 +0100

    bits

--- Runtime/Scripts/Utils/AddPanelStuff.cs
+++ Runtime/Scripts/Utils/AddPanelStuff.cs
@@ -1,21 +1,27 @@
+using System;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class AddPanelStuff : MonoBehaviour
 {
     [SerializeField] private UIDocument uIDocument;
+    
+    private PanelRaycaster _raycaster;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        
         if (uIDocument == null)
             uIDocument = GetComponent<UIDocument>();
         if (uIDocument != null)
         {
-            var raycaster = GetComponent<PanelRaycaster>();
-            if (raycaster != null)
+            _raycaster = GetComponent<PanelRaycaster>();
+            if (_raycaster != null)
             {
                 Debug.Log("[AddPanelStuff] Assigning PanelRaycaster's panel to UIDocument's runtimePanel.");
-                raycaster.panel = uIDocument.runtimePanel;
+                _raycaster.panel = uIDocument.runtimePanel;
             }
 
             var eventHandler = GetComponent<PanelEventHandler>();
@@ -24,6 +30,14 @@ public class AddPanelStuff : MonoBehaviour
                 Debug.Log("[AddPanelStuff] Assigning PanelEventHandler's panel to UIDocument's runtimePanel.");
                 eventHandler.panel = uIDocument.runtimePanel;
             }
+
+            uIDocument.runtimePanel.selectableGameObject = gameObject;
         }
+    }
+
+    private void Update()
+    {
+        if (!_raycaster.enabled)
+            _raycaster.enabled = true;
     }
 }
