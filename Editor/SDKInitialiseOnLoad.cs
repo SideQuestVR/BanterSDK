850e4417b6061fd9b56f28c69ce2d1374aa9d810
shane <shane@sidequestvr.com>
2026-01-29 14:00:19 +0100

    maybe it works now?

--- Editor/SDKInitialiseOnLoad.cs
+++ Editor/SDKInitialiseOnLoad.cs
@@ -138,35 +138,6 @@ namespace Banter.SDKEditor
 //                 File.WriteAllText("Packages/com.sidequest.banteruninstaller/Editor/UninstallBanter.cs", script);
 //             }
 //         }
-#if !BANTER_EDITOR
-        [MenuItem("Banter/Uninstall SDK")]
-        static void UninstallBanter()
-        {
-            RemoveRequest request = Client.Remove("com.sidequest.banter");
-            while(!request.IsCompleted)
-            {
-                
-            }
-            EditUtils.RemoveCompileDefine("BANTER_ORA", new BuildTargetGroup[] { BuildTargetGroup.Android, BuildTargetGroup.Standalone });
-            EditUtils.RemoveCompileDefine("BASIS_BUNDLE_MANAGEMENT", new BuildTargetGroup[] { BuildTargetGroup.Android, BuildTargetGroup.Standalone });
-            if(Directory.Exists("Packages/com.basis.bundlemanagement"))
-            {
-                Directory.Delete("Packages/com.basis.bundlemanagement");
-            }
-            if(Directory.Exists("Packages/com.basis.sdk"))
-            {
-                Directory.Delete("Packages/com.basis.sdk");
-            }
-            if(Directory.Exists("Packages/com.basis.odinserializer"))
-            {
-                Directory.Delete("Packages/com.basis.odinserializer");
-            }
-            if (Directory.Exists("Packages/com.sidequest.ora"))
-            {
-                Directory.Delete("Packages/com.sidequest.ora");
-            }
-        }
-#endif
         static void ImportOraPackage()
         {
             var packageName = "com.sidequest.ora";
